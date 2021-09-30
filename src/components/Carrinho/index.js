5c008726d4b4032cfb2de6030cb49f1dff1ca74c
glen1986 <escobar.glen@gmail.com>
2021-09-30 17:35:47 -0300

    carrinhoValores

--- src/components/Carrinho/index.js
+++ src/components/Carrinho/index.js
@@ -1,5 +1,5 @@
 import {Container} from './styles'
-const Carrinho = ({addCarro, onClick, title, children}) => {
+const Carrinho = ({addCarro}) => {
 
   const valores = addCarro.map(item => item.price).reduce((a, b) => {return a + b}, 0)
   const desconto = addCarro.map(item => item.discount).reduce((a, b) => {return a + b}, 0)
@@ -18,7 +18,6 @@ const Carrinho = ({addCarro, onClick, title, children}) => {
             <td className="description">descri√ßao: {item.description} </td>
             <td className="price">preco: {item.price}, </td>
             <td className="discount" > disconto: {item.discount}, </td>
-            <td ><button onClick={() => onClick(item)}>{children}</button> </td>
           </tr>)}
       </div>
       <div>Valor: {Math.floor(valores)}</div>
