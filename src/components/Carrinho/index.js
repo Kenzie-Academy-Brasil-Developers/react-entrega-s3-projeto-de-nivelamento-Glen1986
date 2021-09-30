7f52cdd04e29f44614a4b92a73b34e4266458c6d
glen1986 <escobar.glen@gmail.com>
2021-09-30 18:02:19 -0300

    carrinhoCharac

--- src/components/Carrinho/index.js
+++ src/components/Carrinho/index.js
@@ -16,7 +16,6 @@ const Carrinho = ({addCarro}) => {
                   <td className="code">code: {item.code} </td>
                   <td className="quant">nome: {item.name} </td>
                   <td className="description">descri&ccedil;&atilde;o: {item.description} </td>
-                  <td className="description">descri&ccedil;ao: {item.description} </td>
                   <td className="price">preco: {item.price} </td>
                   <td className="discount" > disconto: {item.discount} </td>
                </tr>)}
