23d3e2bd3748a234153b7688f3f744afdc392aec
glen1986 <escobar.glen@gmail.com>
2021-09-30 14:58:41 -0300

    carrinho2

--- src/components/Carrinho/index.js
+++ src/components/Carrinho/index.js
@@ -1,26 +1,25 @@
 import {Container} from './styles'
-const Carrinho = ({addCarro, onClick, title, children}) => {
+const Carrinho = ({addCarro, }) => {
 
-  const valores = addCarro.map(item => item.price).reduce((a,b)=> { return a+b },0)
-  const desconto = addCarro.map(item => item.discount).reduce((a,b)=> { return a+b },0)
-  const total=() =>{
+  const valores = addCarro.map(item => item.price).reduce((a, b) => {return a + b}, 0)
+  const desconto = addCarro.map(item => item.discount).reduce((a, b) => {return a + b}, 0)
+  const total = () => {
     return valores - desconto
-  } ;
-  
+  };
+
   return (
     <Container>
       <div className="carrinho">
         <span>Carrinho</span>
-        {addCarro.map((item, index)=> 
+        {addCarro.map((item, index) =>
           <tr key={index}>
-           <td className="code">code: {item.code}, </td>
-           <td className="quant">nome: {item.name}, </td>
-           <td className="description">descri√ßao: {item.description} </td>
-           <td className="price">preco: {item.price}, </td>
-           <td className="discount" > disconto: {item.discount}, </td>
-           <td ><button onClick={()=>onClick(item)}>{children}</button> </td>
-          </tr> )} 
-        </div>
+            <td className="code">code: {item.code}, </td>
+            <td className="quant">nome: {item.name}, </td>
+            <td className="description">descri&ccedil;ao: {item.description} </td>
+            <td className="price">preco: {item.price}, </td>
+            <td className="discount" > disconto: {item.discount}, </td>
+          </tr>)}
+      </div>
       <div>Valor: {Math.floor(valores)}</div>
       <div>Desconto: {Math.floor(desconto)}</div>
       <div>Total: {Math.floor(total())}</div>
