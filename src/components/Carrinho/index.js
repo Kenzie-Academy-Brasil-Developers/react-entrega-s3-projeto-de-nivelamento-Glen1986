78cf95a88b5f768a7c855479e61ed18d39a9af3b
glen1986 <escobar.glen@gmail.com>
2021-10-01 17:19:35 -0300

    filter

--- src/components/Carrinho/index.js
+++ src/components/Carrinho/index.js
@@ -1,29 +1,29 @@
 import {Container} from './styles'
-const Carrinho = ({carro}) => {
+const Carrinho = ({carro, takeOff, children}) => {
 
    const valores = carro.map(item => item.price).reduce((a, b) => {return a + b}, 0)
    const desconto = carro.map(item => item.discount).reduce((a, b) => {return a + b}, 0)
    const total = () => {
       return valores - desconto
    };
-
    return (
       <Container>
          <div className="carrinho">
             <span>Carrinho</span>
             {carro.map((item, index) =>
-               <tr key={index}>
+               <tr id={index} key={index}>
                   <td className="code">code: {item.code} </td>
                   <td className="quant">nome: {item.name} </td>
                   <td className="description">descri&ccedil;&atilde;o: {item.description} </td>
                   <td className="price">preco: {item.price} </td>
                   <td className="discount" > disconto: {item.discount} </td>
+                  <td ClassName="button" id={index}> <button onClick={(e) => takeOff(item.name)} >{children}</button> </td>
                </tr>)}
          </div>
          <div>Valor: {Math.floor(valores)}</div>
          <div>Desconto: {desconto}</div>
          <div>Total: {total().toFixed((2))}</div>
-      </Container>
+      </Container >
    )
 }
 export default Carrinho;
